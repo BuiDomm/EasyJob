8f84dcb1103faae6a49a8904a9d84bdf15b52757
thai-hoang-03 <144097744+thai-hoang-03@users.noreply.github.com>
2024-06-21 09:05:58 +0700

    Update JobApplyDAO.java

--- src/java/dao/JobApplyDAO.java
+++ src/java/dao/JobApplyDAO.java
@@ -10,8 +10,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Apply;
 import model.CVProfile;
+import model.Category;
+import model.Company;
 import model.Job;
 import model.User;
 
@@ -26,18 +30,19 @@ public class JobApplyDAO {
     ResultSet rs = null;
 
     // apply
-    public List<Apply> ListApplyByUserId(int uid) {
+    public List<Apply> ListApplyByUserId(int uid, String statusApplication) {
 
         List<Apply> list = new ArrayList<>();
-        String sql = "	select * from Applications a\n"
+        String sql = "select * from Applications a\n"
                 + "join CVProfile cv on cv.CVId = a.CVId\n"
                 + "join Jobs j on a.JobID = j.JobID\n"
                 + "join CompanyProfile c on j.CompanyID = c.CompanyID\n"
-                + "where c.UserID = ? ";
+                + "where c.UserID = ? and a.Status = ?  ";
         try {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(sql);
             ps.setInt(1, uid);
+            ps.setString(2, statusApplication);
             rs = ps.executeQuery();
             while (rs.next()) {
                 int idApp = rs.getInt(1);
@@ -59,6 +64,61 @@ public class JobApplyDAO {
 
     }
 
+    public int getTotalApply(int uid, String statusApplication) {
+        String query = "select count(*) from Applications a\n"
+                + "join CVProfile cv on cv.CVId = a.CVId\n"
+                + "join Jobs j on a.JobID = j.JobID\n"
+                + "join CompanyProfile c on j.CompanyID = c.CompanyID\n"
+                + "where c.UserID = ? and a.Status = ? ";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, uid);
+            ps.setString(2, statusApplication);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    public List<Apply> pagingCVList(int uid, String statusApplication, int index) {
+        List<Apply> list = new ArrayList<>();
+        String sql = " select * from Applications a\n"
+                + "join CVProfile cv on cv.CVId = a.CVId\n"
+                + "join Jobs j on a.JobID = j.JobID\n"
+                + "join CompanyProfile c on j.CompanyID = c.CompanyID\n"
+                + "where c.UserID = ? and a.Status = ?\n"
+                + "order by a.ApplicationID\n"
+                + "OFFSET ? rows fetch next 2 rows only";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, uid);
+            ps.setString(2, statusApplication);
+            ps.setInt(3, (index - 1) * 2);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int idApp = rs.getInt(1);
+                int jobID = rs.getInt(2);
+                int CVid = rs.getInt(3);
+                Date date = rs.getDate(4);
+                String status = rs.getString(5);
+                JobDAO jd = new JobDAO();
+                Job j = jd.findById(jobID);
+                CVDAO cv = new CVDAO();
+                CVProfile cp = cv.findByID(CVid);
+                Apply a = new Apply(idApp, j, cp, date, status);
+                list.add(a);
+            }
+        } catch (Exception ex) {
+
+        }
+        return list;
+    }
+
     //u
     public User getUserByCVid(int cvid) {
 
@@ -69,6 +129,38 @@ public class JobApplyDAO {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(sql);
             ps.setInt(1, cvid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int idUser = rs.getInt(1);
+                String firstName = rs.getString(2);
+                String lastName = rs.getString(3);
+                String email = rs.getString(4);
+                String password = rs.getString(5);
+                int roleID = rs.getInt(6);
+                String message = rs.getString(7);
+                String city = rs.getString(8);
+                String phoneNumber = rs.getString(9);
+                Date dob = rs.getDate(10);
+                String status = rs.getString(11);
+                User u = new User(idUser, firstName, lastName, email, password, roleID, message, status, city, "0" + phoneNumber, dob);
+                return u;
+
+            }
+        } catch (Exception ex) {
+        }
+        return null;
+    }
+
+    public User getUserByapplyid(int apply) {
+
+        String sql = "select * from Users u\n"
+                + "join CVProfile cv on u.UserID = cv.UserID\n"
+                + "join Applications a on cv.CVId = a.CVId\n"
+                + "where a.ApplicationID = ?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, apply);
             rs = ps.executeQuery();
             while (rs.next()) {
                 int idUser = rs.getInt(1);
@@ -114,44 +206,64 @@ public class JobApplyDAO {
         }
     }
 
-    public List<Apply> ListApplyByUserIdAndJobID(int uid, int jobId) {
-
-        List<Apply> list = new ArrayList<>();
-        String sql = "select * from Applications a "
-                + "join CVProfile cv on cv.CVId = a.CVId "
-                + "join Jobs j on a.JobID = j.JobID "
-                + "join CompanyProfile c on j.CompanyID = c.CompanyID "
-                + "where c.UserID = ? and a.JobID = ? "
-                + "order by a.ApplicationDate DESC";
+    //job
+    public Job findByApplyId(int applyid) {
+        String sql = "select * from jobs j\n"
+                + "join Applications a on j.JobID = a.JobID\n"
+                + "where a.ApplicationID = ?";
         try {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(sql);
-            ps.setInt(1, uid);
-            ps.setInt(2, jobId);
+            ps.setInt(1, applyid);
             rs = ps.executeQuery();
-            while (rs.next()) {
-                int idApp = rs.getInt(1);
-                int jobID = rs.getInt(2);
-                int CVid = rs.getInt(3);
-                Date date = rs.getDate(4);
-                String status = rs.getString(5);
 
-                JobDAO jd = new JobDAO();
-                Job j = jd.findById(jobID);
-                CVDAO cv = new CVDAO();
-                CVProfile cp = cv.findByID(CVid);
-                Apply a = new Apply(idApp, j, cp, date, status);
-                list.add(a);
+            while (rs.next()) {
+                CompanyDAO com = new CompanyDAO();
+                CategoryDAO cd = new CategoryDAO();
+                int idJob = rs.getInt(1);
+                int idCompany = rs.getInt(2);
+                int idCategory = rs.getInt(3);
+                String title = rs.getString(4);
+                String desc = rs.getString(5);
+                int expY = rs.getInt(6);
+                String location = rs.getString(7);
+                int salary = rs.getInt(8);
+                String status = rs.getString(9);
+                Date date = rs.getDate(10);
+                Company company = com.findById(idCompany);
+                Category category = cd.findById(idCategory);
+                Job j = new Job(idJob, company, category, title, desc, expY, location, salary, status, date);
+                return j;
+
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Logger.getLogger(JobseekerDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return list;
+        return null;
+    }
+
+    //notification
+    public void insertNotificationApprovel(int useId, String mess, int status) {
+        String query = "INSERT INTO Notifications( UserID, [Message],ReadStatus)\n"
+                + "VALUES (?,?,?);";
+
+        try {
+            conn = new DBContext().getConnection();//mo ket noi vs sql
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, useId);
+            ps.setString(2, mess);
+            ps.setInt(3, status);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+
     }
 
     public static void main(String[] args) {
         JobApplyDAO dao = new JobApplyDAO();
-        List<Apply> j = dao.ListApplyByUserIdAndJobID(23, 8);
+        List<Apply> j = dao.pagingCVList(2, "Pending", 1);
+        Job a = dao.findByApplyId(1003);
+        System.out.println(a);
         for (Apply cVProfile : j) {
             System.out.println(cVProfile);
         }
